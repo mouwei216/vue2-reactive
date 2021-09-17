2d73ec21a33acbc268169abaa8d0fbe2ab4063cc
mouweimo <mouwei216@gmail.com>
2021-09-17 21:56:21 +0800

    解决问题： Q1：如果obj的key属性的值为数组怎么办？这样第二次进入到observe()时，再对数组的索引进行Object.defineProperty()吗？据我测试Object.defineProperty()对数组的索引进行设置是不生效的哟。
    Q2：如果一个obj已经被观测过怎么办？
    
    Q3：在setter里面，如果新设置的值没有被观测过也该对这个新值进行观测吧
    
    Q4：如果观测完obj后，再在obj添加一个新属性或者删除obj的一个已有的属性怎么办？按道理这个新属性也应该是响应式的吖，按道理如果页面渲染用到了obj的那个要删除的属性，页面也应该要重新渲染吖
    
     A1：确实是这不生效，看来数组得另外搞了。先搞完对象再来搞数组吧。
    
     A2：由于对一个对象进行观测时我们都会new一个Observer实例，所以我们可以用这个实例来标识一个对象是否被观测过。那我们可以设置一个不可以枚举的属性在obj上，这个属性的值就是Observer实例，属性名字就叫__ob__。一来，可以通过这个属性来判断obj是否已被观测过，如果被观测过就不再对其进行观测了。二来，在下面遍历obj所有的key的过程中，可以使这个属性不被遍历出来，从而避免把这个属性也设置成响应式的。
    
     A3：six six six 吖
    
     A4：没有办法来监测obj的属性的添加和删除吖。嗯嗯嗯，搞些替代方法吧，set(obj, key, val)和del(obj, key)，你要想添加或删除一个属性就用这两个方法吧！

--- index.js
+++ index.js
@@ -3,10 +3,31 @@ function isObject(value) {
   return value !== null && typeof value === 'object';
 }
 
+// 把一个key以数据描述符的形式定义到obj上
+function def(obj, key, val, enumerable) {
+  Object.defineProperty(obj, key, {
+    value: val,
+    enumerable: !!enumerable,
+    writable: true,
+    configurable: true,
+  });
+}
+
+// 判断obj自身是否有key属性
+function hasOwn(obj, key) {
+  return obj.hasOwnProperty(key);
+}
+
 // 这个类负责对一数据进行观测
 class Observer {
   constructor(value) {
-    this.walk(value);
+    def(value, '__ob__', this);
+
+    if (Array.isArray(value)) {
+      // todo
+    } else {
+      this.walk(value);
+    }
   }
 
   // 把obj的每一个属性都设置为响应式的
@@ -19,15 +40,18 @@ class Observer {
 }
 
 // 把obj的key属性设置为响应式的
-function defineReactive(obj, key) {
+// 或者为obj添加一个key属性，并且把这个key属性也设置成响应式的
+function defineReactive(obj, key, val) {
   const propertyDescriptor = Object.getOwnPropertyDescriptor(obj, key);
   // obj的key属性不可以重新配置就别瞎搞了
-  if (propertyDescriptor.configurable === false) {
+  if (propertyDescriptor && propertyDescriptor.configurable === false) {
     return;
   }
 
-  // 取出key对应的值
-  let val = obj[key];
+  // 如果是新添加的key，propertyDescriptor为undefined
+  if (propertyDescriptor) {
+    val = obj[key];
+  }
 
   // 如果obj的key属性的值也是个对象，则继续对它的属性进行设置
   observe(val);
@@ -39,11 +63,14 @@ function defineReactive(obj, key) {
       return val;
     },
     set: function reactiveSetter(newVal) {
-      // 新旧值都一样就别瞎搞了吧，val !== val 判断val是否是NaN
+      // 新旧值都一样就别瞎搞了吧
       if (newVal === val || (newVal !== newVal && val !== val)) {
         return;
       }
       val = newVal;
+
+      // 如果新设置的值没有被观测过则要对其进行观测
+      observe(newVal);
 
       console.log('设置了新值，数据发变化了，重新渲染页面吧');
     },
@@ -51,12 +78,54 @@ function defineReactive(obj, key) {
 }
 
 // 对一个对象进行观测，把obj的所有属性都设置为响应式的
-function observe(obj) {
-  // 不是对象就别来瞎搞了
-  if (!isObject(obj)) {
+function observe(value) {
+  // 不是对象或者数组就别来瞎搞了
+  if (!isObject(value)) {
     return;
   }
 
-  // 观测过程都在这个类实例化过程中
-  new Observer(obj);
+  let ob;
+
+  // 已经被观测过了就别来瞎搞了
+  if (hasOwn(value, '__ob__') && value.__ob__ instanceof Observer) {
+    ob = value.__ob__;
+  } else {
+    ob = new Observer(value);
+  }
+
+  return ob;
+}
+
+// 为target添加一个响应式的key属性，
+function set(target, key, val) {
+  // key已经存在，并且不是Object原型的属性
+  if (key in target && !(key in Object.prototype)) {
+    target[key] = val;
+    return;
+  }
+
+  // target没有观测过，那就直接添加
+  if (!target.__ob__) {
+    target[key] = val;
+    return;
+  }
+
+  defineReactive(target, key, val);
+
+  console.log('添加了属性，渲染页面吧');
+}
+
+// 删除target的key属性
+function del(target, key) {
+  if (!target.hasOwnProperty(key)) {
+    return;
+  }
+
+  delete target[key];
+
+  if (!target.__ob__) {
+    return;
+  }
+
+  console.log('删除了属性，渲染页面吧');
 }
