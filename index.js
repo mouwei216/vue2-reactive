6323137d3df4578d31bbd4cd8f689d90b63f82d0
mouweimo <mouwei216@gmail.com>
2021-09-17 22:00:21 +0800

    添加Dep与Watcher

--- index.js
+++ index.js
@@ -241,3 +241,125 @@ function del(target, key) {
 
   console.log('删除了属性，渲染页面吧');
 }
+
+let depUid = 0;
+class Dep {
+  // 唯一标识
+  id;
+
+  // 订阅者们(Watcher)
+  subs;
+  subIds;
+
+  constructor() {
+    this.id = depUid++;
+    this.subs = [];
+    this.subIds = new Set();
+  }
+
+  // 添加一个订阅者
+  addSub(sub) {
+    if (!this.subIds.has(sub.id)) {
+      this.subs.push(sub);
+      this.subIds.add(sub.id);
+    }
+  }
+
+  // 删除一个订阅者
+  removeSub(sub) {
+    if (this.subIds.has(sub.id)) {
+      this.subIds.delete(sub.id);
+      const index = this.subs.indexOf(sub);
+      if (index >= 0) {
+        this.subs.splice(index, 1);
+      }
+    }
+  }
+
+  // 通通知与这个dep关联的订阅者们，属性发生了变化
+  notify() {
+    const subs = this.subs.slice();
+    for (let sub of subs) {
+      sub.update();
+    }
+  }
+}
+
+let watcherUid = 0;
+class Watcher {
+  // 唯一标识
+  id;
+
+  // 渲染页面操作函数是哪个组件的
+  vm;
+
+  // 渲染页面操作函数
+  cb;
+
+  // 依赖
+  // watcher要把自己的依赖存起来，等下一次收集依赖时再与新收集到的依赖作对比，
+  // 看看是否需要取消订阅不再依赖的发布者
+  deps;
+  depIds;
+
+  // 新一轮依赖
+  newDeps;
+  newDepIds;
+
+  constructor(vm, cb) {
+    this.id = watcherUid++;
+    this.vm = vm;
+    this.cb = cb;
+
+    this.deps = [];
+    this.depIds = new Set();
+    this.newDeps = [];
+    this.newDepIds = new Set();
+  }
+
+  update() {
+    this.cb.call(this.vm);
+  }
+
+  // 收集依赖(订阅发布者)
+  depend(dep) {
+    if (this.newDepIds.has(dep.id)) {
+      return;
+    }
+
+    this.newDepIds.add(dep.id);
+    this.newDeps.push(dep);
+
+    if (this.depIds.has(dep.id)) {
+      // 之前已经添加过就不必再添加了
+      return;
+    }
+
+    dep.addSub(this);
+  }
+
+  // 清除上轮用过但现在没用的依赖
+  cleanupDeps() {
+    // 对于旧的dep，在新的上面找，如果没找到则说明新一轮不再依赖这个旧dep
+    for (let dep of this.deps) {
+      if (!this.newDepIds.has(dep.id)) {
+        dep.removeSub(this);
+      }
+    }
+
+    // this.deps = this.newDeps;
+    // this.depIds = this.newDepIds;
+    // this.newDeps = [];
+    // this.newDepIds = new Set();
+
+    // 新旧交换，并清空新的，比上面的快
+    let tmp = this.depIds;
+    this.depIds = this.newDepIds;
+    this.newDepIds = tmp;
+    this.newDepIds.clear();
+    tmp = this.deps;
+    this.deps = this.newDeps;
+    this.newDeps = tmp;
+    this.newDeps.length = 0;
+  }
+}
